48c540fbe47846764730745f48e4c7425b853628
Patricio Pittana <pittanapatricio@gmail.com>
2024-05-16 21:23:03 -0300

    Data desde el json

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,39 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import data from '../data/productos.json'
+import Item from './Item';
 
 const ItemListContainer = (props) => {
-  const clas = "productsContainer";
+  let [productos, setProductos] = useState([]);
   const darkMode = () => {
     props.darkMode === 'claro' ? props.setDarkMode('oscuro') : props.setDarkMode('claro');
   }
 
+  const predirProductos = () => {
+    return new Promise((resolve, reject) => {
+      resolve(data);
+    })
+  }
+
+  useEffect(() => {
+
+    predirProductos()
+      .then((res) => {
+        setProductos(res);
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+
   return (
     <div className={props.darkMode === "claro" ? "claro productsContainer" : "oscuro productsContainer"} >
       <p>{props.greeting}</p>
+      {
+        productos.length > 0 ? productos.map( productos => {
+          return (
+            <Item key={productos.id} producto={productos}/>
+          )
+        }) : "No hay productos"
+      }
       <hr />
       <button onClick={darkMode}>Cambiar modo de color</button>
     </div>
